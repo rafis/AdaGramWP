48a5835d6ea00430b1d4e2c026227cc65f269bb5
glicerico <suarezandres@gmail.com>
2017-07-06 20:04:27 +0800

    simple change in preprocessor

--- utils/preprocess_multiple_files.sh
+++ utils/preprocess_multiple_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#find . -type f -exec /home/andres/.julia/v0.4/AdaGram/utils/tokenize_leaveCaps.sh {} tokenized_{} \;
 find . -type f -exec cat {} + >> allfiles.txt
 /home/andres/.julia/v0.4/AdaGram/utils/tokenize_leaveCaps.sh allfiles.txt tokenized_allfiles.txt
 /home/andres/.julia/v0.4/AdaGram/utils/dictionary.sh tokenized_allfiles.txt dict_allfiles.txt
